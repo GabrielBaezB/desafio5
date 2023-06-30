d438b3dce80e56a6a5fcde67caeab153c25e3e7b
GabrielBaezB <gabooo@live.com>
2023-06-30 06:13:41 +0000

    Cambio en database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_30_010329) do
+ActiveRecord::Schema[7.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "timescaledb"
-
-  create_table "characters", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.string "house"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
